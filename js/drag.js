cfbcf25f0e646fb9ff4019b51d5a1a9b24649fe7
lukejonathanjohnson <lukejohnsonluke@gmail.com>
2022-09-30 13:41:59 +0100

    dragfixed

--- js/drag.js
+++ js/drag.js
@@ -1,5 +1,5 @@
 // Make the DIV element draggable:
-dragElement(document.getElementByClass("windowcomp"));
+dragElement(document.getElementById("windowcomp"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
